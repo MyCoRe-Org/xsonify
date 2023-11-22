854270998bce2e7c9bab11a7359fd9b1c3c2f3d4
Matthias Eichner <matthias.eichner@uni-jena.de>
2023-11-22 16:42:32 +0100

    fix CI

--- xsonify-xsd/src/main/java/org/mycore/xsonify/xsd/node/XsdReferenceable.java
+++ xsonify-xsd/src/main/java/org/mycore/xsonify/xsd/node/XsdReferenceable.java
@@ -4,6 +4,7 @@ public interface XsdReferenceable<T extends XsdNode> {
 
     T getReference();
 
+    @SuppressWarnings("unchecked")
     default T getReferenceOrSelf() {
         T reference = getReference();
         return reference != null ? reference : (T) this;
